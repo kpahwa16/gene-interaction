f42f2f943e459e7a5d1b7f124bdfb204b064d1ad
KHUSHBU PAHWA <56195814+kpahwa16@users.noreply.github.com>
2023-11-17 00:30:40 +0530

    Create models.py

---
+++ code/models.py
@@ -0,0 +1,25 @@
+#mlp baseline 
+class MLP(nn.Module):
+    def __init__(self, num_genes, hidden1=2048, hidden2=512):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(num_genes, hidden1),
+            nn.Softplus(),
+            nn.Linear(hidden1, hidden2),
+            nn.Softplus(),
+            nn.Linear(hidden2, 2)  # Two output neurons for binary classification
+        )
+
+    def forward(self, x):
+        '''Forward pass'''
+        return self.layers(x)  # Output raw logits for CrossEntropyLoss
+
+    def forward_plus(self, x):
+        '''Forward pass with intermediate layer access'''
+        x1 = self.layers[0](x)
+        y = self.layers[1:](x1)
+        return y, x1
+
+    def layer1_weight(self):
+        return self.layers[0].weight
+
