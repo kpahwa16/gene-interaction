e66a67d411a094b49a36df5a3eee71dc7fe0f494
KHUSHBU PAHWA <56195814+kpahwa16@users.noreply.github.com>
2023-11-17 12:51:25 +0530

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -18,10 +18,9 @@ def save_interaction_scores(model, loader, output_filename):
 # Save interaction scores for test data
 # Usage: save_interaction_scores(mlp_model, test_loader, "interaction_scores.csv")
 
-
+#optimized the code below
 def get_second_order_grad(model, x, class_idx, num_genes):
     with torch.set_grad_enabled(True):
-
         if x.nelement() < 2:
             return np.array([])
 
@@ -29,22 +28,19 @@ def get_second_order_grad(model, x, class_idx, num_genes):
         output, x1 = model.forward_plus(x)
         y = torch.gather(output, 1, class_idx).sum(dim=0)
 
-        grads = autograd.grad(y, x1, create_graph=True, allow_unused=True)[0].sum(dim=0)  # [valid_index]
+        grads = autograd.grad(y, x1, create_graph=True, allow_unused=True)[0].sum(dim=0)
 
-        grad_list = []
-        for j, grad in enumerate(grads):
-            grad2 = autograd.grad(grad, x1, retain_graph=True)[0]  # .sum(dim=0)  # [valid_index]
-            grad_list.append(grad2.detach())
+        # This loop is a potential bottleneck
+        grad_list = [autograd.grad(grad, x1, retain_graph=True)[0].detach() for grad in grads]
 
     with torch.no_grad():
-
         grad_matrix = torch.stack(grad_list).permute((1, 0, 2))
         del grad_list
         grad_matrix0 = torch.zeros((num_genes, num_genes), device=x.device)
         weight1 = model.layer1_weight()
 
-        for idx in range(grad_matrix.shape[0]):
-            grad_matrix0 += weight1.T.mm(grad_matrix[idx]).mm(weight1).abs()
+        # Vectorized operation to reduce loop iterations
+        grad_matrix0 += weight1.T.mm(grad_matrix.sum(dim=0)).mm(weight1).abs()
 
     return grad_matrix0
 
