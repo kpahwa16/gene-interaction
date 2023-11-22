27e20f6b767988bd55b4e973c58196b59e990eee
KHUSHBU PAHWA <56195814+kpahwa16@users.noreply.github.com>
2023-11-23 01:19:07 +0530

    Update modified_models.py

--- set_transformer/modified_models.py
+++ set_transformer/modified_models.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from modified_modules import *
+# from modified_modules import *
 
 class ModifiedSetTransformer(nn.Module):
     def __init__(self, dim_input, num_outputs, dim_output, num_inds=32, dim_hidden=128, num_heads=4, ln=False):
@@ -17,22 +17,39 @@ class ModifiedSetTransformer(nn.Module):
         self.attention_weights = []
 
     def forward(self, X, mask=None):
+        print("Input X shape:", X.shape)
+        print("Input mask shape:", mask.shape if mask is not None else "No mask")
         self.attention_weights = []
 
-        # Manually handle sequential processing for encoder
-        for layer in self.enc:
-            X = layer(X, mask)
-
-        # Manually handle sequential processing for decoder
+        # Process through the first encoder layer
+        X = self.enc1(X, mask)
+        print("After enc1 X shape:", X.shape)
+        # Process through the second encoder layer
+        X = self.enc2(X, mask)
+        print("After enc2 X shape:", X.shape)
+        # Initialize decoder output
         dec_out = X
-        for layer in self.dec:
-            if isinstance(layer, (SAB, PMA)):
-                dec_out, attn_weight = layer(dec_out, mask, return_attention=True)
-                self.attention_weights.append(attn_weight)
-            else:
-                dec_out = layer(dec_out)
+        print("dec_out initialized shape:", dec_out.shape)
+
+        # Process through the first decoder layer
+        dec_out, attn_weight = self.dec1(dec_out, mask, return_attention=True)
+        print("After dec1 dec_out shape:", dec_out.shape)
+        self.attention_weights.append(attn_weight)
+
+        # Process through the second decoder layer
+        dec_out, attn_weight = self.dec2(dec_out, mask, return_attention=True)
+        print("After dec2 dec_out shape:", dec_out.shape)
+        self.attention_weights.append(attn_weight)
+
+        # Process through the third decoder layer
+        dec_out, attn_weight = self.dec3(dec_out, mask, return_attention=True)
+        print("After dec3 dec_out shape:", dec_out.shape)
+        self.attention_weights.append(attn_weight)
+
+        # Apply the final linear layer
+        dec_out = self.final_linear(dec_out)
+        print("After final_linear dec_out shape:", dec_out.shape)
 
         return dec_out
         
-        
-        
+
