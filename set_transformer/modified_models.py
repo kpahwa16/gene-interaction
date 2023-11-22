309c89c3938ec62715ea1a446a499cb5824c6f0f
KHUSHBU PAHWA <56195814+kpahwa16@users.noreply.github.com>
2023-11-22 19:50:43 +0530

    Update modified_models.py

--- set_transformer/modified_models.py
+++ set_transformer/modified_models.py
@@ -7,20 +7,32 @@ from modified_modules import *
 class ModifiedSetTransformer(nn.Module):
     def __init__(self, dim_input, num_outputs, dim_output, num_inds=32, dim_hidden=128, num_heads=4, ln=False):
         super(ModifiedSetTransformer, self).__init__()
-        self.enc = nn.Sequential(
-            ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln),
-            ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln))
-        self.dec = nn.Sequential(
-            PMA(dim_hidden, num_heads, num_outputs, ln=ln),
-            SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
-            SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
-            nn.Linear(dim_hidden, dim_output))
+        # Define encoder and decoder as individual components
+        self.enc1 = ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln)
+        self.enc2 = ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln)
+        self.dec1 = PMA(dim_hidden, num_heads, num_outputs, ln=ln)
+        self.dec2 = SAB(dim_hidden, dim_hidden, num_heads, ln=ln)
+        self.dec3 = SAB(dim_hidden, dim_hidden, num_heads, ln=ln)
+        self.final_linear = nn.Linear(dim_hidden, dim_output)
         self.attention_weights = []
 
     def forward(self, X, mask=None):
         self.attention_weights = []
-        # Passing the mask through the encoder and decoder
-        enc_out = self.enc(X, mask)
-        dec_out, attn_weight = self.dec(enc_out, mask, return_attention=True)
-        self.attention_weights.append(attn_weight)
+
+        # Manually handle sequential processing for encoder
+        for layer in self.enc:
+            X = layer(X, mask)
+
+        # Manually handle sequential processing for decoder
+        dec_out = X
+        for layer in self.dec:
+            if isinstance(layer, (SAB, PMA)):
+                dec_out, attn_weight = layer(dec_out, mask, return_attention=True)
+                self.attention_weights.append(attn_weight)
+            else:
+                dec_out = layer(dec_out)
+
         return dec_out
+        
+        
+        
